77c9a41c37c3abcf546fb3b11a01c7be7cf22f75
badman-dev <budmandev@gmail.com>
2023-10-24 16:47:21 -0400

    file saving

--- TagExtractor/src/ExtractorFrame.java
+++ TagExtractor/src/ExtractorFrame.java
@@ -2,8 +2,10 @@ import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -11,11 +13,14 @@ import java.util.Scanner;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import static java.nio.file.StandardOpenOption.CREATE;
+
 public class ExtractorFrame extends JFrame {
     Map<String, Integer> printMap;
     ArrayList<String> noiseWords;
     ArrayList<String> filteredTextWords;
     File textFile;
+    FileNameExtensionFilter filter = new FileNameExtensionFilter("Text files (.txt)", "txt");
 
     JPanel topPnl;
     JPanel midPnl;
@@ -27,6 +32,8 @@ public class ExtractorFrame extends JFrame {
     JTextArea displayArea;
     JScrollPane displayScroll;
 
+    JButton saveBtn;
+
     public ExtractorFrame() {
         createTopPanel();
         createMiddlePanel();
@@ -36,7 +43,7 @@ public class ExtractorFrame extends JFrame {
         Dimension screenSize = tk.getScreenSize();
 
         setTitle("Extractor");
-        setSize(800, 800);
+        setSize(480, 640);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setVisible(true);
@@ -65,7 +72,11 @@ public class ExtractorFrame extends JFrame {
 
     private void createBottomPanel() {
         botPnl = new JPanel();
+        saveBtn = new JButton("Save Results");
 
+        saveBtn.addActionListener((ActionEvent ae) -> saveStats());
+
+        botPnl.add(saveBtn);
         add(botPnl, BorderLayout.SOUTH);
     }
 
@@ -93,12 +104,14 @@ public class ExtractorFrame extends JFrame {
             Scanner myReader = new Scanner(textFile);
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
-                String[] lineWords = data.replaceAll(
-                        "[^a-zA-Z\\s]", "").toLowerCase().split(" ");
-                for (String word : lineWords) {
-                    System.out.println(word);
-                    if (!noiseWords.contains(word)) {
-                        filteredTextWords.add(word);
+                if (!data.isEmpty()) {
+                    String[] lineWords = data.replaceAll(
+                            "[^a-zA-Z\\s]", "").toLowerCase().split(" ");
+                    for (String word : lineWords) {
+                        System.out.println(word);
+                        if (!noiseWords.contains(word)) {
+                            filteredTextWords.add(word);
+                        }
                     }
                 }
             }
@@ -124,14 +137,42 @@ public class ExtractorFrame extends JFrame {
     private File fileOpener()
     {
         JFileChooser chooser = new JFileChooser();
-        FileNameExtensionFilter filter = new FileNameExtensionFilter("Text files (.txt)", "txt");
         chooser.setFileFilter(filter);
-        int returnVal = chooser.showOpenDialog(this);
-        if (returnVal == JFileChooser.APPROVE_OPTION) {
+        if (chooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
             System.out.println("You chose to open this file: " +
                     chooser.getSelectedFile().getName());
             return chooser.getSelectedFile();
         }
         return null;
+    }
+
+    private void saveStats() {
+        JFileChooser chooser = new JFileChooser();
+        chooser.setFileFilter(filter);
+        if (chooser.showSaveDialog(this) == JFileChooser.APPROVE_OPTION) {
+            File file = chooser.getSelectedFile();
+            if (!file.toString().endsWith(".txt")) {
+                file = new File(file.toString() + ".txt");
+            }
+
+            String content = displayArea.getText();
+            Scanner in = new Scanner(System.in);
+            Path filePath = Paths.get(file.getPath());
+
+            try
+            {
+                OutputStream out =
+                        new BufferedOutputStream(Files.newOutputStream(filePath, CREATE));
+                BufferedWriter writer =
+                        new BufferedWriter(new OutputStreamWriter(out));
+                writer.write(content, 0, content.length());
+                writer.close(); // must close the file to seal it and flush buffer
+                System.out.println("Data file written!");
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
     }
 }
